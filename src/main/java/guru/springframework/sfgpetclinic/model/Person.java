a68bb4410b4e844988777726cc9b12bee78af013
Nirmal Kumar <nirmalpm@gmail.com>
2018-12-01 10:23:49 +0530

    Implement pet clinic POJO Data model adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLasstName(String lastName) {
+		this.lastName = lastName;
+	}
+}
