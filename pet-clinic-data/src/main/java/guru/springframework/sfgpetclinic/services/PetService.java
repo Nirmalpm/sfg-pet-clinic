a8229ec2ece579dea558a70609c5f542f872a72f
Nirmal Kumar <nirmalpm@gmail.com>
2018-12-02 12:30:02 +0530

    Refactor Service interface to common base interface Closes #11.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,12 +1,7 @@
 package guru.springframework.sfgpetclinic.services;
 
-import java.util.Set;
-
-import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 
-public interface PetService {
-	Pet findById(Pet pet);
-	Pet save(Pet pet);
-	Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
+	
 }
