e2595f2f601782db3623b2c99c801820a6d1cd74
Nirmal Kumar <nirmalpm@gmail.com>
2018-12-03 16:00:25 +0530

    Load Bootstrap data on startup. Closes #15

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -2,12 +2,18 @@ package guru.springframework.sfgpetclinic.model;
 
 public class Person extends BaseEntity{
 	private String firstName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 	private String lastName;
 	
 	public String getLastName() {
 		return lastName;
 	}
-	public void setLasstName(String lastName) {
+	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 }
